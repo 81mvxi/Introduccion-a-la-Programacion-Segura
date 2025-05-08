bfe3c2cc3a3837925036eaf367ca192696d43360
mvxi88 <mgarrido0k@gmail.com>
2025-05-07 20:09:23 -0400

    Primera clase para aprender

--- Introduccion-A-La-Programaci-n/general/Datos_Genericos.py
+++
@@ -1,6 +0,0 @@
-import math
-
-raiz_25 = math.sqrt(25)
-print(math.trunc(raiz_25)) #"trunc" es aproximar o de un resultado como 5.0, lo trunca y da 5, sin el ".0"
-sumar_1_15 = math.fsum(range(0,15))
-print(math.trunc(sumar_1_15)) 
